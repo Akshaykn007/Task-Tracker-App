13e7222bbf98c4f50a53d3d30609a783576acc38
akshaykumar.r <akshaykumar.r@tmachinesoftware.com>
2022-04-20 11:41:46 +0530

    Added celery settings

--- taskTracker/settings.py
+++ taskTracker/settings.py
@@ -168,3 +168,11 @@ SIMPLE_JWT = {
 
 # for sending default mails
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+
+# Celery Broker - Redis  
+CELERY_BROKER_URL = 'redis://localhost:6379'  
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'  
+CELERY_ACCEPT_CONTENT = ['application/json']  
+CELERY_TASK_SERIALIZER = 'json'  
+CELERY_RESULT_SERIALIZER = 'json'  
+CELERY_TIMEZONE = "Asia/Kolkata"
--- taskTrackerApp/worker/email_sender.py
+++ taskTrackerApp/worker/email_sender.py
@@ -1,28 +1,32 @@
+from __future__ import absolute_import, unicode_literals
 from django.contrib.auth.models import User
 from taskTrackerApp.models import *
 from taskTrackerApp.libraries.library import *
+from celery import shared_task
+import datetime,time
 
 
 
-
-
-def email_send(deadlineEmailList,emailList):
-    
+@shared_task(name="sending_email")
+def email_send():
     #at the end of the day send this mail to team leaders
-    send_mail('Status Report For The Day', 'Hi sir plz check the report for the day', 'a4akshaykn@gmail.com', emailList,fail_silently=False)
-    send_mail('Status Report For The Day', 'Hi sir plz check the report for the day', 'a4akshaykn@gmail.com', deadlineEmailList,fail_silently=False)
+    emailList=[]
+    deadlineEmailList=[]
+    users=User.objects.all()
+    for i in users:
+            emailList.append(i.email)
+    dateToday=datetime.datetime.now().date()
+    for things in Task.objects.all():
+        if things.end_date < dateToday:
+            for fields in Members.objects.all():
+                if fields.member == things.team_member:
+                    deadlineEmailList.append(fields.email)
+    deadlineEmailList=deadlineEmailList+emailList
+    print("hello")
+    try:
+        send_mail('Status Report For The Day', 'Hi sir plz check the report for the day', 'unknown@gmail.com', emailList,fail_silently=False)
+        send_mail('Deadline closing!!!!!!', 'Hi team, plz finish the task as early as possible!', 'unknown@gmail.com', deadlineEmailList,fail_silently=False)
+    except:
+        pass
+    # time.sleep(60)
     return True
-
-emailList=[]
-deadlineEmailList=[]
-users=User.objects.all()
-for i in users:
-        emailList.append(i.email)
-dateToday=datetime.datetime.now().date()
-for things in Task.objects.all():
-    if things.end_date < dateToday:
-        for fields in Members.objects.all():
-            if fields.member == things.team_member:
-                deadlineEmailList.append(fields.email)
-deadlineEmailList=deadlineEmailList+emailList
-email_send(deadlineEmailList,emailList)
